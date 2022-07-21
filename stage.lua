432a66788095bc7a5ecf4ed5c3d4d8f288e2886d
xerocuil <tbrodhead@mm.st>
2022-07-21 12:09:35 -0400

    2022.0721.1209

---
+++ stage.lua
@@ -0,0 +1,22 @@
+-- ## Set Variables
+local AI = require 'ai'
+local Background = require 'background'
+local Ball = require 'ball'
+
+-- ## Init Stage
+Stage = {}
+
+-- ## Load
+function Stage:load()
+	
+end
+
+-- ## Update
+function Stage:update(dt)
+
+end
+
+-- ## Draw
+function Stage:draw()
+
+end
